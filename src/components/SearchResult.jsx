36003fcaa47f7aec33fb23f254757c692d4c45b1
0305940221 <ulabideenzain77@gmail.com>
2023-10-20 00:42:24 +0500

    initialcommit

---
+++ src/components/SearchResult.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./SearchResult.css"
+
+const SearchResult = ({result}) => {
+  return (
+    <div className='search-result' onClick={(e)=>alert(`you clicked on ${result.name}`)}>{result.name}</div>
+  )
+}
+
+export default SearchResult
